313864801a707f8552a14ca888d0619cb94780ae
zulal <github-zulal.polat@outlook.com>
2026-01-10 11:35:11 +0300

    harf bulma algoritmasi

--- hello.py
+++ hello.py
@@ -1,2 +1,13 @@
-print("Merhaba Github")
 
+
+word=input("Lütfen bir kelime giriniz: ")
+x=input("aramak istediğiniz harfi giriniz: ")
+
+for i in word:
+ 
+
+ if i==x:
+  print("kelime aradiginiz harfi icerir.")
+  break
+else:
+  print("kelime aradiginiz harfi icermez."
